c0354f1686e535ffa04a0dae6e1556d799905707
Jack <anisse33k@hotmail.fr>
2018-09-10 16:53:59 +0200

    First commit

---
+++ src/main/java/calculator.java
@@ -0,0 +1,23 @@
+public class calculator {
+
+    public static int add(int a , int b)
+    {
+
+        if (a > 0 && b > 0 && a+b <=0)
+            throw new IndexOutOfBoundsException();
+
+        return a+b;
+    }
+
+    public static int subtract(int a , int b)
+    {
+
+        return 0;
+
+    }
+
+
+}
+
+
+
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCalculator {
+
+@Test
+    public void test1plus1()
+    {
+        ///AAA
+        //arrange = organiser
+        int arg1 = 1;
+        int arg2 = 1;
+        int expectedResult = 2 ;
+
+        //act = action
+        int returnedResult = calculator.add(arg1,arg2);
+
+        //Assert
+        Assert.assertEquals(expectedResult, returnedResult);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+
+    public void testMax()
+    {
+        ///AAA
+        //arrange = organiser
+        int arg1 = Integer.MAX_VALUE;
+        int arg2 = 1;
+
+        //act = action
+        int returnedResult = calculator.add(arg1,arg2);
+
+
+    }
+
+
+
+
+}
